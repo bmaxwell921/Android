571644e8cf3fcd8bdd448348601225e32cccb1c6
Brandon <bmaxwell921@gmail.com>
2014-03-25 10:45:01 -0500

    Implemented the simple game tutorial

--- mygame/android/src/com/me/mygame/android/MainActivity.java
+++ mygame/android/src/com/me/mygame/android/MainActivity.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.me.mygame.MyGame;
+import com.me.mygame.Drop;
 
-public class AndroidLauncher extends AndroidApplication {
+public class MainActivity extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGame(), config);
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new Drop(), config);
 	}
 }
--- mygame/desktop/src/com/me/mygame/desktop/Main.java
+++ mygame/desktop/src/com/me/mygame/desktop/Main.java
@@ -2,11 +2,14 @@ package com.me.mygame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.me.mygame.MyGame;
+import com.me.mygame.Drop;
 
-public class DesktopLauncher {
+public class Main {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGame(), config);
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Drop(), config);
 	}
 }
