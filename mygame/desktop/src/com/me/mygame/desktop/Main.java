ce4af7eb5d85ffedd0bdac421fb10f77363c7071
Brandon <bmaxwell921@gmail.com>
2014-03-25 23:24:09 -0500

    Messing with texture packing

--- mygame/desktop/src/com/me/mygame/desktop/Main.java
+++ mygame/desktop/src/com/me/mygame/desktop/Main.java
@@ -2,14 +2,25 @@ package com.me.mygame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2;
+import com.badlogic.gdx.tools.imagepacker.TexturePacker2.Settings;
 import com.me.mygame.Drop;
 
 public class Main {
 	public static void main (String[] arg) {
+		runPacker();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Drop";
 		config.width = 800;
 		config.height = 480;
 		new LwjglApplication(new Drop(), config);
+	}
+	
+	private static void runPacker() {
+		Settings settings = new Settings();
+//		settings.maxWidth = 512;
+//		settings.maxHeight = 512;
+		
+		TexturePacker2.process(settings, "../images", "../android/assets", "game");
 	}
 }
